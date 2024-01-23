5ceeb26b35470ae113942214090a110e01fba0e3
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2024-01-23 21:09:52 +0330

    refactor: update project dependencies

--- app/[locale]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function Notfound() {
-    return (
-        <div className="bg-[#eaf6f9] min-h-screen flex justify-center items-center">
-            <Image height={450} width={450} src={"/images/404.gif"} alt="404" />
-        </div>
-    )
-}
